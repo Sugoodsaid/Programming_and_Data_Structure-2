33d6620c0c06976a789b6cd724c99d64cc328fe0
Sugoodsaid <saja82303@gmail.com>
2024-11-08 16:51:59 +0200

    Create 1-square.py
    Add Square class definition files
    
    - Implemented 1-square.py with a Square class containing a private size attribute.
    - Ensured encapsulation for size to maintain attribute integrity.

---
+++ classes/1-square.py
@@ -0,0 +1,7 @@
+class Square:
+    """Class that defines a square with a private size attribute."""
+    
+    def __init__(self, size):
+        self.__size = size  # Private instance attribute
+
+  
